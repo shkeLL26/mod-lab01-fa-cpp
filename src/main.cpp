1c616a5f7a8083cc5fd8d3d26a8f1515e6e794af
shkeLL26 <egsh26@gmail.com>
2026-03-29 01:38:07 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,10 @@
 #include "fun.h"
 
 int main() {
+    const char* testStr = "Hello, world!";
+    std::cout << testStr << "\n";
+    std::cout << faStr1(testStr) << "\n";
+    std::cout << faStr2(testStr) << "\n";
+    std::cout << faStr3(testStr);
     return 0;
 }
